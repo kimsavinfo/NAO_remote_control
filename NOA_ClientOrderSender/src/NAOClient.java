066beac8b0a8ad3ac00d6695de155facf88c8db5
Kim SAVAROCHE <kimsavinfo@air-de-kim.home>
2015-04-25 18:07:38 +0200

    sockets OP + clean up

---
+++ NOA_ClientOrderSender/src/NAOClient.java
@@ -0,0 +1,49 @@
+/**
+ * Created by kimsavinfo on 11/11/14.
+ */
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class NAOClient
+{
+    public static void main(String[] args)
+    {
+        Socket socket;
+
+        try
+        {
+
+            socket = new Socket("192.168.1.29",1234);
+
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            String sendMessage = "123#say#Hello World!$";
+            // String sendMessage = "123#sitDown#$";
+            // String sendMessage = "123#standUp#$";
+
+            //String sendMessage = number + "\n";
+            bw.write(sendMessage);
+            bw.flush();
+            System.out.println("Message envoyé au serveur : "+sendMessage);
+            socket.close();
+
+        }catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        }catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
