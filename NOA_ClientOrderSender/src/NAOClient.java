2aff3a70f979a6e96181c348f9734bb05cee0e20
Kim SAVAROCHE <kimsavinfo@air-de-kim.home>
2015-04-26 02:51:01 +0200

    move to OP

--- NOA_ClientOrderSender/src/NAOClient.java
+++ NOA_ClientOrderSender/src/NAOClient.java
@@ -24,9 +24,17 @@ public class NAOClient
             OutputStreamWriter osw = new OutputStreamWriter(os);
             BufferedWriter bw = new BufferedWriter(osw);
 
-            String sendMessage = "123#say#Hello World!$";
+            // String sendMessage = "123#say#Hello World!$";
             // String sendMessage = "123#sitDown#$";
             // String sendMessage = "123#standUp#$";
+
+
+            // NOTe peut composer : avancer et tourner à gauche
+            // String sendMessage = "123#walk#0.1#0.0#0.0$"; // avancer
+            // String sendMessage = "123#walk#0.0#0.0#10$"; // tourner à gauche
+            // String sendMessage = "123#walk#0.0#0.0#-10$"; // tourner à droite
+
+            String sendMessage = "123#goodbye#$";
 
             //String sendMessage = number + "\n";
             bw.write(sendMessage);
